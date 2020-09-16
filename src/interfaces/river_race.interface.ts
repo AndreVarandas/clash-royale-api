110b4e3886f42f72b2fca1b0e6f879260926fa64
firegore <firegore2@gmail.com>
2020-09-16 23:01:18 +0200

    Added current rive race endpoint and interfaces

---
+++ src/interfaces/river_race.interface.ts
@@ -0,0 +1,23 @@
+export interface ICurrentRiverRace {
+    state: string
+    clan: IRiverRaceClan
+    clans: IRiverRaceClan[]
+    sectionIndex: number
+}
+
+export interface IRiverRaceClan {
+    tag: string
+    clanScore: number
+    badgeId: number
+    name: string
+    fame: number
+    repairPoints: number
+    finishTime: string
+    participants: IRiverRaceParticipant[]
+}
+export interface IRiverRaceParticipant {
+    fame: number
+    name: string
+    repairPoints: number
+    tag: string
+}
